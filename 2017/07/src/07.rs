5891075a7a6793f5a7427e51de64f637529dee72
BlockOG <alesiobaca@gmail.com>
2023-12-05 18:29:32 +0200

    2017/07/2

--- 2017/07/src/07.rs
+++ 2017/07/src/07.rs
@@ -1,7 +1,91 @@
-use aoc_parse::{parser, prelude::*};
-use std::collections::{hash_map::Entry, HashMap};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    mem,
+};
 
-aoc::parts!(1);
+use aoc::{IterUnwrap, Parse};
+use parse_display::FromStr;
+
+aoc::parts!(1, 2);
+
+#[derive(FromStr, Default)]
+#[from_str(regex = r"(?<name>[a-z]+) \((?<weight>\d+)\)( -> (?<disk>[a-z]+(, [a-z]+)*))?")]
+#[from_str(new = Self::new(name, weight, disk))]
+struct ParsedProgram {
+    name: String,
+    weight: u32,
+    disk: Vec<String>,
+}
+
+impl ParsedProgram {
+    fn new(name: String, weight: u32, disk: String) -> Self {
+        Self {
+            name,
+            weight,
+            disk: if disk.is_empty() {
+                vec![]
+            } else {
+                disk.split(", ").map(|i| i.to_owned()).collect()
+            },
+        }
+    }
+}
+
+struct Program {
+    weight: u32,
+    total_weight: u32,
+    disk: Vec<Program>,
+}
+
+impl Program {
+    fn new(weight: u32, disk: Vec<Program>) -> Self {
+        Self {
+            weight,
+            total_weight: weight + disk.iter().map(|i| i.total_weight).sum::<u32>(),
+            disk,
+        }
+    }
+
+    fn recurse(&self, value: u32) -> u32 {
+        if self.disk.len() == 1 {
+            self.disk[0].recurse(0);
+        }
+
+        let mut first_value: Option<(u32, bool, &Program)> = None;
+        let mut second_value: Option<(u32, &Program)> = None;
+        for i in self.disk.iter() {
+            match first_value {
+                Some((value, ref mut multiple, _)) => {
+                    if i.total_weight == value {
+                        if let Some((_, program)) = second_value {
+                            return program.recurse(value);
+                        }
+                        *multiple = true;
+                    } else {
+                        match second_value {
+                            Some((value, _)) => {
+                                if let Some((_, _, program)) = first_value {
+                                    return program.recurse(value);
+                                }
+                            }
+                            None => {
+                                if *multiple {
+                                    return i.recurse(value);
+                                }
+                                second_value = Some((i.total_weight, i))
+                            }
+                        }
+                    }
+                }
+                None => {
+                    first_value = Some((i.total_weight, false, i));
+                }
+            }
+        }
+
+        value - (self.total_weight - self.weight)
+    }
+}
 
 fn part_1(input: aoc::Input) -> impl ToString {
     let mut pr = HashMap::new();
@@ -23,13 +107,27 @@ fn part_1(input: aoc::Input) -> impl ToString {
 }
 
 fn part_2(input: aoc::Input) -> impl ToString {
-    let p = parser!(string(lower+) " (" u32 ")" (" -> " repeat_sep(string(lower+), ", "))?);
+    let mut programs: Vec<ParsedProgram> = input.lines().map(|i| i.parse_uw()).collect();
 
-    // let mut
-    // for i in input.lines() {
+    let mut hm = HashMap::new();
+    while !programs.is_empty() {
+        programs.retain_mut(|program| {
+            for i in program.disk.iter() {
+                if !hm.contains_key(i) {
+                    return true;
+                }
+            }
 
-    // }
-    0
+            let ParsedProgram { name, weight, disk } = mem::take(program);
+
+            let disk = disk.into_iter().map(|i| hm.remove(&i).unwrap()).collect();
+            hm.insert(name, Program::new(weight, disk));
+
+            false
+        });
+    }
+
+    hm.into_iter().next_uw().1.recurse(0)
 }
 
 fn lower_strings(string: &str) -> Vec<&str> {

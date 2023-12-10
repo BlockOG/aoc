5816871d4f7af1d6b09c0040bbb5ab7ccee1be62
BlockOG <alesiobaca@gmail.com>
2023-12-10 19:48:24 +0200

    optimize 2023/10

--- 2023/10/src/10.rs
+++ 2023/10/src/10.rs
@@ -1,109 +1,282 @@
 aoc::parts!(1, 2);
 
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum Tile {
-    Start,
-    Ground,
+const UP: u8 = 0b0001;
+const DOWN: u8 = 0b0010;
+const LEFT: u8 = 0b0100;
+const RIGHT: u8 = 0b1000;
 
-    Vertical,
-    Horizontal,
-
-    TopRight,
-    TopLeft,
-
-    BottomRight,
-    BottomLeft,
-}
-
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+const CHAR_TO_PIPE: [u8; 256] = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    LEFT | RIGHT,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    DOWN | LEFT,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    DOWN | RIGHT,
+    0,
+    0,
+    0,
+    UP | LEFT,
+    0,
+    UP | RIGHT,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    UP | DOWN,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+];
 
 fn part_1(input: aoc::Input) -> impl ToString {
     let n = input[0].len();
     let m = input.len();
 
-    let mut grid = vec![Tile::Ground; n * m];
-    let mut start = (0, 0);
-    for (y, i) in input.lines().enumerate() {
-        for (x, i) in i.bytes().enumerate() {
-            if i == b'S' {
-                start = (x, y);
-            }
+    let direction_to_offset = [0, -(n as isize), n as isize, 0, -1, 0, 0, 0, 1];
 
-            grid[x + y * n] = match i {
-                b'S' => Tile::Start,
-                b'.' => Tile::Ground,
-
-                b'|' => Tile::Vertical,
-                b'-' => Tile::Horizontal,
-
-                b'L' => Tile::TopRight,
-                b'J' => Tile::TopLeft,
-
-                b'F' => Tile::BottomRight,
-                b'7' => Tile::BottomLeft,
-
-                _ => unreachable!(),
-            }
-        }
-    }
-
-    let mut curr = start;
-    let mut direction;
-    if curr.1 > 0
-        && matches!(
-            grid[curr.0 + (curr.1 - 1) * n],
-            Tile::BottomLeft | Tile::Vertical | Tile::BottomRight
-        )
-    {
-        curr.1 -= 1;
-        direction = Direction::Up;
-    } else if curr.1 < grid.len() - 1
-        && matches!(
-            grid[curr.0 + (curr.1 + 1) * n],
-            Tile::TopLeft | Tile::Vertical | Tile::TopRight
-        )
-    {
-        curr.1 += 1;
-        direction = Direction::Down;
-    } else if curr.0 > 0
-        && matches!(
-            grid[curr.0 - 1 + curr.1 * n],
-            Tile::BottomRight | Tile::Horizontal | Tile::TopRight
-        )
-    {
-        curr.0 -= 1;
-        direction = Direction::Left;
-    } else {
-        curr.0 += 1;
-        direction = Direction::Right;
-    }
+    let (grid, start, mut curr, mut direction) = parse(n, m, input);
+    curr = curr.wrapping_add_signed(direction_to_offset[direction as usize]);
 
     let mut i = 1;
     while curr != start {
-        (curr, direction) = match (grid[curr.0 + curr.1 * n], direction) {
-            (Tile::Vertical, Direction::Up) => ((curr.0, curr.1 - 1), Direction::Up),
-            (Tile::Vertical, Direction::Down) => ((curr.0, curr.1 + 1), Direction::Down),
-            (Tile::Horizontal, Direction::Left) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::Horizontal, Direction::Right) => ((curr.0 + 1, curr.1), Direction::Right),
-
-            (Tile::TopRight, Direction::Down) => ((curr.0 + 1, curr.1), Direction::Right),
-            (Tile::TopRight, Direction::Left) => ((curr.0, curr.1 - 1), Direction::Up),
-            (Tile::TopLeft, Direction::Down) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::TopLeft, Direction::Right) => ((curr.0, curr.1 - 1), Direction::Up),
-
-            (Tile::BottomRight, Direction::Up) => ((curr.0 + 1, curr.1), Direction::Right),
-            (Tile::BottomRight, Direction::Left) => ((curr.0, curr.1 + 1), Direction::Down),
-            (Tile::BottomLeft, Direction::Up) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::BottomLeft, Direction::Right) => ((curr.0, curr.1 + 1), Direction::Down),
-
-            _ => unreachable!(),
-        };
-
+        direction = grid[curr] ^ swap_odd_even_u4(direction);
+        curr = curr.wrapping_add_signed(direction_to_offset[direction as usize]);
         i += 1;
     }
 
@@ -114,85 +287,17 @@ fn part_2(input: aoc::Input) -> impl ToString {
     let n = input[0].len();
     let m = input.len();
 
-    let mut grid = vec![Tile::Ground; n * m];
-    let mut start = (0, 0);
-    for (y, i) in input.lines().enumerate() {
-        for (x, i) in i.bytes().enumerate() {
-            if i == b'S' {
-                start = (x, y);
-            }
+    let direction_to_offset = [0, -(n as isize), n as isize, 0, -1, 0, 0, 0, 1];
 
-            grid[x + y * n] = match i {
-                b'S' => Tile::Start,
-                b'.' => Tile::Ground,
-
-                b'|' => Tile::Vertical,
-                b'-' => Tile::Horizontal,
-
-                b'L' => Tile::TopRight,
-                b'J' => Tile::TopLeft,
-
-                b'F' => Tile::BottomRight,
-                b'7' => Tile::BottomLeft,
-
-                _ => unreachable!(),
-            }
-        }
-    }
-
-    let mut curr = start;
-    let mut direction;
-    if curr.1 > 0
-        && matches!(
-            grid[curr.0 + (curr.1 - 1) * n],
-            Tile::BottomLeft | Tile::Vertical | Tile::BottomRight
-        )
-    {
-        curr.1 -= 1;
-        direction = Direction::Up;
-    } else if curr.1 < grid.len() - 1
-        && matches!(
-            grid[curr.0 + (curr.1 + 1) * n],
-            Tile::TopLeft | Tile::Vertical | Tile::TopRight
-        )
-    {
-        curr.1 += 1;
-        direction = Direction::Down;
-    } else if curr.0 > 0
-        && matches!(
-            grid[curr.0 - 1 + curr.1 * n],
-            Tile::BottomRight | Tile::Horizontal | Tile::TopRight
-        )
-    {
-        curr.0 -= 1;
-        direction = Direction::Left;
-    } else {
-        curr.0 += 1;
-        direction = Direction::Right;
-    }
+    let (grid, start, mut curr, mut direction) = parse(n, m, input);
+    curr = curr.wrapping_add_signed(direction_to_offset[direction as usize]);
 
     let mut in_loop = vec![false; n * m];
-    in_loop[start.0 + start.1 * n] = true;
+    in_loop[start] = true;
     while curr != start {
-        in_loop[curr.0 + curr.1 * n] = true;
-        (curr, direction) = match (grid[curr.0 + curr.1 * n], direction) {
-            (Tile::Vertical, Direction::Up) => ((curr.0, curr.1 - 1), Direction::Up),
-            (Tile::Vertical, Direction::Down) => ((curr.0, curr.1 + 1), Direction::Down),
-            (Tile::Horizontal, Direction::Left) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::Horizontal, Direction::Right) => ((curr.0 + 1, curr.1), Direction::Right),
-
-            (Tile::TopRight, Direction::Down) => ((curr.0 + 1, curr.1), Direction::Right),
-            (Tile::TopRight, Direction::Left) => ((curr.0, curr.1 - 1), Direction::Up),
-            (Tile::TopLeft, Direction::Down) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::TopLeft, Direction::Right) => ((curr.0, curr.1 - 1), Direction::Up),
-
-            (Tile::BottomRight, Direction::Up) => ((curr.0 + 1, curr.1), Direction::Right),
-            (Tile::BottomRight, Direction::Left) => ((curr.0, curr.1 + 1), Direction::Down),
-            (Tile::BottomLeft, Direction::Up) => ((curr.0 - 1, curr.1), Direction::Left),
-            (Tile::BottomLeft, Direction::Right) => ((curr.0, curr.1 + 1), Direction::Down),
-
-            _ => unreachable!(),
-        };
+        in_loop[curr] = true;
+        direction = grid[curr] ^ swap_odd_even_u4(direction);
+        curr = curr.wrapping_add_signed(direction_to_offset[direction as usize]);
     }
 
     let mut res = 0;
@@ -200,10 +305,7 @@ fn part_2(input: aoc::Input) -> impl ToString {
         let mut evenodd = 0;
         for x in 0..n {
             if in_loop[x + y * n] {
-                if matches!(
-                    grid[x + y * n],
-                    Tile::TopLeft | Tile::Vertical | Tile::TopRight
-                ) {
+                if grid[x + y * n] & UP != 0 {
                     evenodd += 1;
                 }
             } else {
@@ -213,4 +315,42 @@ fn part_2(input: aoc::Input) -> impl ToString {
     }
 
     res
+}
+
+fn parse(n: usize, m: usize, input: aoc::Input) -> (Vec<u8>, usize, usize, u8) {
+    let mut grid = vec![0; n * m];
+    let mut start = 0;
+    for (y, i) in input.lines().enumerate() {
+        for (x, i) in i.bytes().enumerate() {
+            if i == b'S' {
+                start = x + y * n;
+            }
+
+            grid[x + y * n] = CHAR_TO_PIPE[i as usize];
+        }
+    }
+
+    let mut direction = 0;
+    if start >= n && grid[start - n] & DOWN != 0 {
+        direction = UP;
+        grid[start] |= direction;
+    }
+    if start < n * (m - 1) && grid[start + n] & UP != 0 {
+        direction = DOWN;
+        grid[start] |= direction;
+    }
+    if start > 0 && grid[start - 1] & RIGHT != 0 {
+        direction = LEFT;
+        grid[start] |= direction;
+    }
+    if start < n * m - 1 && grid[start + 1] & LEFT != 0 {
+        direction = RIGHT;
+        grid[start] |= direction;
+    }
+
+    (grid, start, start, direction)
+}
+
+fn swap_odd_even_u4(a: u8) -> u8 {
+    ((a & 0b0101) << 1) | ((a & 0b1010) >> 1)
 }
